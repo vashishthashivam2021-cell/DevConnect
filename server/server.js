b29885d83d406880e40fba0e6ae5957d585da6d6
Shivam Vashishtha <vashishtha.shivam2021@gmail.com>
2026-03-06 04:11:15 +0530

    Added few pages and some features

--- server/server.js
+++ server/server.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("../client"));
+
 app.post("/login", (req, res) => {
+  const { username, password } = req.body;
   console.log(req.body);
-  res.send("login purpose");
+  res.sendFile(path.join(__dirname, "../client/feed.html"));
 });
+
 app.post("/register", (req, res) => {
-  res.send("register purpose");
+   const { username, password } = req.body;
+   console.log(req.body);
+   res.sendFile(path.join(__dirname, "../client/feed.html"));
 });
+
+app.post("/AddPost",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../client/AddPost.html"))
+})
+
+app.post("/Thoughts",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../client/Thoughts.html"))
+})
 app.listen(8080, () => {
   console.log("server is running at port 8080");
 });
+
 // const mongoose = require("mongoose");
 // require("dotenv").config();
-
 // const PORT = process.env.PORT || 5000;
-
 // mongoose.connect(process.env.MONGO_URI, {
 //   serverSelectionTimeoutMS: 30000
 // })
