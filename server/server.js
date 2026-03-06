78f849d3754209f5efe3dc29f4feeba7e5003f0c
Shivam Vashishtha <vashishtha.shivam2021@gmail.com>
2026-03-06 18:19:29 +0530

    worked on the login and register data with database

--- server/server.js
+++ server/server.js
@@ -1,21 +1,57 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
+const User = require("./models/user");
 
+const connectDB = require("./config/db");
+
+connectDB();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("../client"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client")));
 
-app.post("/login", (req, res) => {
+
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  console.log(req.body);
-  res.sendFile(path.join(__dirname, "../client/feed.html"));
+ const matched= await User.findOne({username});
+
+ if(!matched){
+  return res.send("you have to registered first")
+ }
+
+ else if(matched.password!==password){
+return res.send("incorrect password")
+ }
+ else {
+res.sendFile(path.join(__dirname, "../client/feed.html"));
+ } 
 });
 
-app.post("/register", (req, res) => {
-   const { username, password } = req.body;
-   console.log(req.body);
-   res.sendFile(path.join(__dirname, "../client/feed.html"));
+
+app.post("/register", async (req,res)=>{
+
+  const {username,password} = req.body;
+
+  const existingUser = await User.findOne({username});
+
+  if(existingUser){
+     return res.send("Username already exists");
+  }
+
+  const newUser = new User({username,password});
+  await newUser.save();
+
+  res.send("success");
+
 });
+
+
+// app.post("/register", (req, res) => {
+//    const { username, password } = req.body;
+//    console.log(req.body);
+//    res.sendFile(path.join(__dirname, "../client/feed.html"));
+// });
 
 app.post("/AddPost",(req,res)=>{
     res.sendFile(path.join(__dirname,"../client/AddPost.html"))
@@ -24,10 +60,13 @@ app.post("/AddPost",(req,res)=>{
 app.post("/Thoughts",(req,res)=>{
     res.sendFile(path.join(__dirname,"../client/Thoughts.html"))
 })
-app.listen(8080, () => {
-  console.log("server is running at port 8080");
-});
 
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running");
+});
 // const mongoose = require("mongoose");
 // require("dotenv").config();
 // const PORT = process.env.PORT || 5000;
